93cc4f57f6998e62b7bbbed06964d099623e8ebc
mayurmamtani <106254698+mayurmamtani@users.noreply.github.com>
2024-04-28 12:21:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 import re
+import textblob
+from textblob import TextBlob
+import cleantext
+
 lemmatizer=WordNetLemmatizer()
 
 nltk.download("all")
@@ -29,11 +33,11 @@ def preprocess_data(data):
 
 # navigation bar
 st.sidebar.image("images/nlp.jpg",width=500)
-st.sidebar.title("Feedback Analyzer \n \n Developed by :  Gaurav Shrivastava & Mayur Mamtani ")
+st.sidebar.title("Feedback Analyzer \n \n Developed by :  Gaurav Shrivastav & Mayur Mamtani ")
 
 
 #file uploader 
-file=st.sidebar.file_uploader("Upload CSV File here",type=["csv"],accept_multiple_files=False)
+upl=st.sidebar.file_uploader("Upload CSV File here",type=["csv"],accept_multiple_files=False)
 
 # submit button
 sub_btn=st.sidebar.button("Submit")
@@ -46,48 +50,75 @@ txt=st.text_input("Enter your feedback here")
 predict_btn=st.button("Predict")
 clear_btn=st.button("Clear")
 
+
+pre=st.text_input('Clean Text:')
+if pre:
+    st.write(cleantext.clean(pre,clean_all=False,extra_spaces=True, stopwords=True, lowercase=True, numbers=True, punct=True))
+
+
 # function to create temporary file from their binary objects
 def create_file(file_bytes,file_name):
 
     with open("tmp_files/"+file_name, 'wb') as f:
         f.write(file_bytes)
 
-if sub_btn:
-    #name of uploaded file
-    file_name=None
-    try:
-        file_name=file.name
-    except:
-        pass
-    if file_name:
-        file_bytes=file.getvalue()
-       # creating temorary files for resumes and jd from their respective binary objects
-        create_file(file_bytes,file_name)
+# if sub_btn:
+#     #name of uploaded file
+#     file_name=None
+#     try:
+#         file_name=file.name
+#     except:
+#         pass
+#     if file_name:
+#         file_bytes=file.getvalue()
+#        # creating temorary files for resumes and jd from their respective binary objects
+#         create_file(file_bytes,file_name)
 
-        df=pd.read_csv("tmp_files/"+file_name)
-        X=df["Feedback"]
-        X=preprocess_data(X)
-        count_x=count.transform(X)
-        y_pred=rf.predict(count_x)
-        y_pred=pd.DataFrame(y_pred)
-        y_pred.columns=["Predicted"]
-        df["Predicted"]=y_pred["Predicted"]
-        df.drop(columns=["Sentiment"],inplace=True,axis=1)
-        df.to_csv("outputs/"+file_name,index=False)
-        csv=df.to_csv().encode('utf-8')
-        st.download_button(
-            label="Download Results",
-            data=csv,
-            file_name='results.csv',
-            mime='text/csv',
- )
-        st.success("Output file is saved in outputs folder")
+#         df=pd.read_csv("tmp_files/"+file_name)
+#         X=df["Feedback"]
+#         X=preprocess_data(X)
+#         count_x=count.transform(X)
+#         y_pred=rf.predict(count_x)
+#         y_pred=pd.DataFrame(y_pred)
+#         y_pred.columns=["Predicted"]
+#         df["Predicted"]=y_pred["Predicted"]
+#         df.drop(columns=["Sentiment"],inplace=True,axis=1)
+#         df.to_csv("outputs/"+file_name,index=False)
+#         csv=df.to_csv().encode('utf-8')
+#         st.download_button(
+#             label="Download Results",
+#             data=csv,
+#             file_name='results.csv',
+#             mime='text/csv',
+#  )
+#         st.success("Output file is saved in outputs folder")
 
 
        
+#     else:
+#         # warning message if no file is uploaded
+#         st.warning("Please upload a csv file")
+
+def score(x):
+    blob1=TextBlob(x)
+    return blob1.sentiment.polarity
+
+
+def analyze(x):
+    if x>=0.5:
+        return 'Positive'
+    elif x<= -0.20:
+        return 'Negative'
     else:
-        # warning message if no file is uploaded
-        st.warning("Please upload a csv file")
+        return 'Neutral'
+
+
+
+if upl:
+    df=pd.read_csv(upl)
+    df['score']=df['Feedback'].apply(score)
+    df['analysis']=df['score'].apply(analyze)
+    st.write(df.head(10))
 
  
 if predict_btn:
@@ -104,6 +135,10 @@ if predict_btn:
             image_name="images/pos.jpg"
 
         result_image=st.image(image=image_name,width=500)
+
+        blob=TextBlob(txt)
+        st.write('Polarity:', round(blob.sentiment.polarity,2))
+        st.write('Subjectivity:',round(blob.sentiment.subjectivity,2))
     else:
         st.warning("Please Enter a Feedback to Predict")
 
